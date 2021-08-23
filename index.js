22060d28bcacc8914ccc5b4b9b66a3a280192559
arnflooff <arnold.florendo.official@gmail.com>
2021-08-22 22:36:08 -0700

    map zips to rateAreas

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 
+const zips = new Map(); // e.g. {'36749': 'AL 11', ...}
 fs.createReadStream('data/zips.csv')
   .pipe(csv())
   .on('data', (data) => {
-    console.log(JSON.stringify(data));
+    const zipcode = data.zipcode; // e.g. '36749'
+    const rateArea = data.state + ' ' + data.rate_area; // e.g. 'AL 11'
+    if (!zips.has(zipcode)) {
+      zips.set(zipcode, rateArea);
+    } else {
+      if (zips.get(zipcode) !== rateArea) { // e.g. '36272' has 'AL 1' and 'AL 13'
+        zips.set(zipcode, null); // 36272
+      }
+    } // e.g. '36005' has 'AL 13' 3 times
   })
   .on('end', () => {
+    console.log(zips.size); // 38804
   });
