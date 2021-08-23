dbdf28a29195874190e93da4c386750c089ee652
arnflooff <arnold.florendo.official@gmail.com>
2021-08-22 22:51:22 -0700

    map plans' rateAreas to rates

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const csv = require('csv-parser');
 const fs = require('fs');
+const path = require('path');
+const dataDir = 'data';
 
 const zips = new Map(); // e.g. {'36749': 'AL 11', ...}
-fs.createReadStream('data/zips.csv')
+fs.createReadStream(path.join(dataDir, 'zips.csv'))
   .pipe(csv())
   .on('data', (data) => {
     const zipcode = data.zipcode; // e.g. '36749'
@@ -17,4 +19,20 @@ fs.createReadStream('data/zips.csv')
   })
   .on('end', () => {
     console.log(zips.size); // 38804
+  });
+
+const plans = new Map(); // e.g. {'GA 7': [298.62, 285.07, ...], ...}
+fs.createReadStream(path.join(dataDir, 'plans.csv'))
+  .pipe(csv())
+  .on('data', (data) => {
+    if (data.metal_level !== 'Silver')
+      return;
+    const rateArea = data.state + ' ' + data.rate_area;
+    if (!plans.has(rateArea)) {
+      plans.set(rateArea, new Set());
+    }
+    plans.get(rateArea).add(parseFloat(data.rate));
+  })
+  .on('end', () => {
+    console.log(plans.size);
   });
